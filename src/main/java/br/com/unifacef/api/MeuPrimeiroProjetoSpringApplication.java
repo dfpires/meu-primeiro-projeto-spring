262016804395113901f477237c63765692493630
Daniel Facciolo Pires <daniel@unifacef.edu.br>
2019-09-17 16:51:14 -0300

    versão com JPA funcionando

--- src/main/java/br/com/unifacef/api/MeuPrimeiroProjetoSpringApplication.java
+++ src/main/java/br/com/unifacef/api/MeuPrimeiroProjetoSpringApplication.java
@@ -1,30 +1,45 @@
 package br.com.unifacef.api;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import br.com.unifacef.api.utils.SenhaUtils;
+import br.com.unifacef.api.entities.Empresa;
+import br.com.unifacef.api.repositories.EmpresaRepository;
 
 @SpringBootApplication
 public class MeuPrimeiroProjetoSpringApplication {
+	@Autowired
+	private EmpresaRepository empresaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjetoSpringApplication.class, args);
-		System.out.println("Alteramos o arquivo application.properties ");
-		System.out.println("de modo que não precisemos configurar agora a fonte de dados do MySQL ");
 	}
 
-	@Bean
-	public CommandLineRunner commandLineRunner() {
-		return args -> {
-			String senhaEncoded = SenhaUtils.gerarBCrypt("123456");
-			System.out.println("Senha encoded: " + senhaEncoded);
-			senhaEncoded = SenhaUtils.gerarBCrypt("123456");
-			System.out.println("Senha encoded novamente:" + senhaEncoded);
-			System.out.println("Senha válida: " + SenhaUtils.senhaValida("123456", 
-					senhaEncoded));
-		};
-	}
+@Bean
+public CommandLineRunner commandLineRunner() {
+return args -> {
+	Empresa empresa = new Empresa();
+	empresa.setRazaoSocial("Depto de Computação");
+	empresa.setCnpj("456");
+	this.empresaRepository.save(empresa);
+	List<Empresa> empresas = empresaRepository.findAll();
+	empresas.forEach(System.out::println);
+	
+	Empresa empresaDb = empresaRepository.findById(1L).orElse(null);
+	System.out.println("Empresa por ID: " + empresaDb);
+	
+	empresaDb.setRazaoSocial("Uni-FACEF Centro Universitário");
+	this.empresaRepository.save(empresaDb);
+	
+	Empresa empresaCnpj = empresaRepository.findByCnpj("123");
+	System.out.println("Empresa por CNPJ: " + empresaCnpj);
+	
+	this.empresaRepository.delete(empresaCnpj);
+	empresas = empresaRepository.findAll();
+	empresas.forEach(System.out::println);
+};
+}
 }
